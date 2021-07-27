1ee73b11ce87b98c83e06e400dad344518949e1c
Sai-Varnitha-Reddy <68200915+Sai-Varnitha-Reddy@users.noreply.github.com>
2021-07-28 01:19:54 +0530

    Update food.java

--- food.java
+++ food.java
@@ -7,16 +7,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Dell
- */
 public class food extends javax.swing.JFrame {
 
     /**
