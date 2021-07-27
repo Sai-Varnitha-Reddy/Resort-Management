8e109a152fa3720eec86635810462dc2c5dc6e24
Sai-Varnitha-Reddy <68200915+Sai-Varnitha-Reddy@users.noreply.github.com>
2021-07-28 01:19:27 +0530

    Update activities.java

--- activities.java
+++ activities.java
@@ -1,13 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Dell
- */
 import java.sql.*;
 import java.sql.PreparedStatement;
 import javax.swing.*;
